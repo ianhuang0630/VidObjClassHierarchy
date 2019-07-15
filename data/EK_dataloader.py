f094ae30b97cc23c85bc40a14455b92b90f0ff20
Ian Huang <iyh2110@columbia.edu>
2019-07-14 20:45:28 -0700

    dataloader with reminder

--- data/EK_dataloader.py
+++ data/EK_dataloader.py
@@ -22,8 +22,8 @@ if DEBUG:
 def resize_transform(d):
     assert 'frames' in d, 'need "frames" in d'
     new_d = d.copy()
-    # TODO 
-    new_d['frames'] = cv2.resize() 
+    # TODO
+    new_d['frames'] = cv2.resize()
     return new_d
 
 def default_filter_function(d):
@@ -44,14 +44,14 @@ class EK_Dataset_pretrain(Dataset):
         self.knowns = knowns
         self.unknowns = unknowns
         self.transform = transform
-        self.class_key_df = pd.read_csv(class_key_path) 
+        self.class_key_df = pd.read_csv(class_key_path)
         # TODO: using the key, convert strings into unkowns
         self.class_key_dict = dict(zip(self.class_key_df.class_key, self.class_key_df.noun_id))
         self.noun_dict = dict(zip(self.class_key_df.noun_id, self.class_key_df.class_key))
 
-        self.DF = DatasetFactory(knowns, unknowns, 
-                    object_data_path, action_data_path, class_key_path)        
-        
+        self.DF = DatasetFactory(knowns, unknowns,
+                    object_data_path, action_data_path, class_key_path)
+
         self.dataset = self.DF.get_dataset()
         assert 'unknown_frame2bbox' in self.dataset \
                 and 'known_frame2bbox' in self.dataset, 'frame2bbox conversion not found'
@@ -70,7 +70,7 @@ class EK_Dataset_pretrain(Dataset):
         participant_id = sample_dict['participant_id']
         start_frame = sample_dict['start_frame']
         end_frame = sample_dict['end_frame']
-        
+
         a = start_frame
         frames = []
         while a < end_frame:
@@ -92,25 +92,25 @@ class EK_Dataset_pretrain(Dataset):
                 this_bbox = np.array(ast.literal_eval(valid_candidates[0]['bbox']))
                 # crop gt_bbox
                 y, x, yd, xd = this_bbox[0]
-                image_black = np.zeros_like(image) 
+                image_black = np.zeros_like(image)
                 image_black[y: y+yd , x:x+xd, : ] = image[y:y+yd, x:x+xd, :]
                 frames.append(image_black)
             a += 30
-        frames = np.stack(frames, axis=3) # T x W x H x C
+        frames = np.stack(frames, axis=3) # T x W x H x C # TODO: reshape needed?
         # get position in the tree
-        encoding = get_tree_position(self.noun_dict[sample_dict['noun_class']], self.knowns) 
+        encoding = get_tree_position(self.noun_dict[sample_dict['noun_class']], self.knowns)
         if encoding is None:
-            top_levels = tuple(get_tree_position(self.noun_dict[sample_dict['noun_class']], self.unknowns)[:-1])  
+            top_levels = tuple(get_tree_position(self.noun_dict[sample_dict['noun_class']], self.unknowns)[:-1])
             assert top_levels in self.unknown_lowest_level_label
             encoding = np.array(list(top_levels)+[self.unknown_lowest_level_label[top_levels][0]])
-        
-        d = {'frames': frames, 
-             'noun_label': self.noun_dict[sample_dict['noun_class']], 
+
+        d = {'frames': frames,
+             'noun_label': self.noun_dict[sample_dict['noun_class']],
              'hierarchy_encoding': encoding}
         if self.transform is not None:
             d = self.transform(d)
-        return d  
- 
+        return d
+
 
 class EK_Dataset(Dataset):
     def __init__(self, knowns, unknowns,
@@ -122,19 +122,19 @@ class EK_Dataset(Dataset):
             transform=None):
         # purpose of the dataset object: either for pretraining or for training/testing
         super(EK_Dataset, self).__init__()
-        
+
         self.image_data_folder = image_data_folder
         self.knowns = knowns
         self.unknowns = unknowns
         self.transform = transform
-        self.class_key_df = pd.read_csv(class_key_path) 
+        self.class_key_df = pd.read_csv(class_key_path)
         # TODO: using the key, convert strings into unkowns
         self.class_key_dict = dict(zip(self.class_key_df.class_key, self.class_key_df.noun_id))
         self.noun_dict = dict(zip(self.class_key_df.noun_id, self.class_key_df.class_key))
 
-        self.DF = DatasetFactory(knowns, unknowns, 
-                    object_data_path, action_data_path, class_key_path)        
-        
+        self.DF = DatasetFactory(knowns, unknowns,
+                    object_data_path, action_data_path, class_key_path)
+
         self.dataset = self.DF.get_dataset()
         assert 'unknown_frame2bbox' in self.dataset \
                 and 'known_frame2bbox' in self.dataset, 'frame2bbox conversion not found'
@@ -143,7 +143,7 @@ class EK_Dataset(Dataset):
         self.training_unknowns = self.dataset['unknown']
         self.known_f2bbox = self.dataset['known_frame2bbox']
         self.unknown_f2bbox = self.dataset['unknown_frame2bbox']
-         
+
         # merging both of the f2bbox's
         self.f2bbox = {}
         for frame in list(set(self.known_f2bbox.keys()).union(set(self.unknown_f2bbox.keys()))):
@@ -158,10 +158,10 @@ class EK_Dataset(Dataset):
         for idx, sample in enumerate(self.training_data):
             if not filter_function(sample):
                 self.training_data.pop(idx)
-        random.shuffle(self.training_data) 
-        
+        random.shuffle(self.training_data)
+
         # prepare tree
-        self.unknown_lowest_level_label = survey_tree(self.knowns)  
+        self.unknown_lowest_level_label = survey_tree(self.knowns)
 
     def __len__(self):
         return len(self.training_data)
@@ -172,7 +172,7 @@ class EK_Dataset(Dataset):
         participant_id = sample_dict['participant_id']
         start_frame = sample_dict['start_frame']
         end_frame = sample_dict['end_frame']
-        
+
         a = start_frame
         frames = []
         gt_bbox = []
@@ -198,19 +198,19 @@ class EK_Dataset(Dataset):
         frames = np.stack(frames, axis=3) # T x W x H x C
         gt_bbox = np.stack(frames, axis=1)  # T x 4
         # get position in the tree
-        encoding = get_tree_position(self.noun_dict[sample_dict['noun_class']], self.knowns) 
+        encoding = get_tree_position(self.noun_dict[sample_dict['noun_class']], self.knowns)
         if encoding is None:
-            top_levels = tuple(get_tree_position(self.noun_dict[sample_dict['noun_class']], self.unknowns)[:-1])  
+            top_levels = tuple(get_tree_position(self.noun_dict[sample_dict['noun_class']], self.unknowns)[:-1])
             assert top_levels in self.unknown_lowest_level_label
             encoding = np.array(list(top_levels)+[self.unknown_lowest_level_label[top_levels][0]])
-        
-        d = {'frames': frames, 'bboxes': gt_bbox, 
-             'noun_label': self.noun_dict[sample_dict['noun_class']], 
+
+        d = {'frames': frames, 'bboxes': gt_bbox,
+             'noun_label': self.noun_dict[sample_dict['noun_class']],
              'hierarchy_encoding': encoding}
         if self.transform is not None:
             d = self.transform(d)
-        return d  
-        
+        return d
+
 
 if __name__=='__main__':
 
@@ -234,14 +234,14 @@ if __name__=='__main__':
     assert os.path.exists(train_object_csvpath), "{} does not exist".format(train_object_csvpath)
     assert os.path.exists(visual_images_folderpath), "{} does not exist".format(visual_images_folderpath)
     assert os.path.exists(class_key_csvpath), "{} does not exist".format(class_key_csvpath)
-        
+
     image_data_folder = os.path.join(visual_images_folderpath, 'train')
-    knowns = ['pan', 'onion'] 
+    knowns = ['pan', 'onion']
     unknowns = ['plate', 'meat']
-    DF = EK_Dataset(knowns, unknowns, 
+    DF = EK_Dataset(knowns, unknowns,
             train_object_csvpath, train_action_csvpath, class_key_csvpath, image_data_folder)
     # print(DF[2])
-        
-    DF_pretrain = EK_Dataset_pretrain(knowns, unknowns, 
+
+    DF_pretrain = EK_Dataset_pretrain(knowns, unknowns,
             train_object_csvpath, train_action_csvpath, class_key_csvpath, image_data_folder)
     print(DF_pretrain[30])
