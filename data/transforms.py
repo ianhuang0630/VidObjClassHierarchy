8faf47257cf124a8c2ebebe6a86b1c49c8006790
Ian Huang <iyh2110@columbia.edu>
2019-07-16 13:54:22 -0700

    spacing

--- data/transforms.py
+++ data/transforms.py
@@ -7,70 +7,70 @@ import cv2
 import numpy as np
 
 class Transpose(object):
-	def __init__(self):
-		pass
-	def __call__(self, d):
-		# 1080, 1920, 3, 11 -->  3, 11, 1080, 1920
-		assert 'frames' in d, 'need "frames" in d'
-		new_d = d.copy()
-		new_d['frames'] = new_d['frames'].transpose([2, 3, 0, 1])
-		return new_d
+    def __init__(self):
+        pass
+    def __call__(self, d):
+        # 1080, 1920, 3, 11 -->  3, 11, 1080, 1920
+        assert 'frames' in d, 'need "frames" in d'
+        new_d = d.copy()
+        new_d['frames'] = new_d['frames'].transpose([2, 3, 0, 1])
+        return new_d
 
 class Rescale(object):
-	def __init__(self, output_size):
-		"""
-		Args: 
-			output_size: size per frame
-		"""
-		self.output_size = output_size
+    def __init__(self, output_size):
+        """
+        Args: 
+            output_size: size per frame
+        """
+        self.output_size = output_size
 
-	def __call__(self, d):
-	    assert 'frames' in d, 'need "frames" in d'
-	    new_d = d.copy()
-	    # transpose first
-	    # 1080, 1920, 3, 11 -->  11, 1080, 1920, 3
-	    frames = new_d['frames']
-	    frames = frames.transpose([3,0,1,2])
-	    frames_resized = []
-	    for frame in frames:
-	    	frames_resized.append(cv2.resize(frame, self.output_size))
-	    frames_resized = np.stack(frames_resized, axis=3)
+    def __call__(self, d):
+        assert 'frames' in d, 'need "frames" in d'
+        new_d = d.copy()
+        # transpose first
+        # 1080, 1920, 3, 11 -->  11, 1080, 1920, 3
+        frames = new_d['frames']
+        frames = frames.transpose([3,0,1,2])
+        frames_resized = []
+        for frame in frames:
+            frames_resized.append(cv2.resize(frame, self.output_size))
+        frames_resized = np.stack(frames_resized, axis=3)
 
-	    # TODO
-	    # reshaping and resizing every single image
-	    new_d['frames'] = frames_resized
-	    return new_d
+        # TODO
+        # reshaping and resizing every single image
+        new_d['frames'] = frames_resized
+        return new_d
 
 class TimeNormalize(object):
-	def __init__(self, num_frames):
-		self.num_frames = num_frames
+    def __init__(self, num_frames):
+        self.num_frames = num_frames
 
-	def __call__(self, d):
-		# reformatting the d['frames'] and normalizing it 
-		# assuming tha tth frames are of shape
-		# 11 1080 1920 3
-		new_d = d.copy()
+    def __call__(self, d):
+        # reformatting the d['frames'] and normalizing it 
+        # assuming tha tth frames are of shape
+        # 11 1080 1920 3
+        new_d = d.copy()
         frames = new_d['frames'].transpose([1, 0, 2, 3])
-		new_d['frames']= frames[np.round(np.linspace(0,len(frames)-1, num=self.num_frames)).astype(np.int), :, :, :]
+        new_d['frames']= frames[np.round(np.linspace(0,len(frames)-1, num=self.num_frames)).astype(np.int), :, :, :]
         new_d['frames'] = new_d['frames'].transpose([1, 0, 2, 3])
-		return new_d
+        return new_d
 
 class ToTensor(object):
-	def __init__(self):
-		pass
-	def __call__(self, d):
-		new_d = d.copy()
-		for key_ in new_d:
-			if type(new_d[key_]) is np.ndarray:
-				new_d[key_] = torch.from_numpy(new_d[key_])
-		return new_d
+    def __init__(self):
+        pass
+    def __call__(self, d):
+        new_d = d.copy()
+        for key_ in new_d:
+            if type(new_d[key_]) is np.ndarray:
+                new_d[key_] = torch.from_numpy(new_d[key_])
+        return new_d
 
 if __name__=='__main__':
     # first rescale
-	# transpose 
-	# timenormalize
-	composed_transfs = transforms.Compose([Rescale((200,200)),
-											Transpose(),
-											TimeNormalize(10),
-											ToTensor()])
+    # transpose 
+    # timenormalize
+    composed_transfs = transforms.Compose([Rescale((200,200)),
+                                            Transpose(),
+                                            TimeNormalize(10),
+                                            ToTensor()])
 
